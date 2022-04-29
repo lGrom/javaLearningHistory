65aeeebc721b8259ac83a2349ab304399c138344
Grom <person.japescapes@gmail.com>
2022-04-29 12:13:02 -0500

    Migrated to intelliJ

--- src/objectOrientedProgramming/polyMorphism/Main.java
+++ src/objectOrientedProgramming/polyMorphism/Main.java
@@ -1,20 +1,25 @@
 package objectOrientedProgramming.polyMorphism;
 
 public class Main {
-	public static void main (String[] args) {
-	
-		Car car = new Car();
-		Bicycle bike = new Bicycle();
-		Boat boat = new Boat();
-		
-		
-		// They all "identify" as vehicles as well as their individual names
-		Vehicle[] racers = {car, bike, boat};
-	
-		
-		for(Vehicle i : racers) {
-			i.go();
-		}
-	
+//	public static void main (String[] args) {
+//
+//		Car car = new Car();
+//		Bicycle bike = new Bicycle();
+//		Boat boat = new Boat();
+//
+//
+//		// They all "identify" as vehicles as well as their individual names
+//		Vehicle[] racers = {car, bike, boat};
+//
+//
+//		for(Vehicle i : racers) {
+//			i.go();
+//		}
+//
+//	}
+
+	public static void main(String[] args){
+
+
 	}
 }
